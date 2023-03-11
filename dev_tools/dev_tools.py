d14428d302d93cbffdf5423b7c881f53decf1229
elamhut <elamhut@gmail.com>
2023-03-11 18:38:42 -0300

    LOD Tool working! Checkpoint

--- dev_tools/dev_tools.py
+++ dev_tools/dev_tools.py
@@ -1,7 +1,9 @@
-from pathlib import Path
 import bpy
-from bpy.types import Operator
-from ..helpers import is_wip_in_path
+from bpy.types import Operator, PropertyGroup, Scene
+from bpy.props import IntProperty, FloatProperty
+
+class BITCAKE_PROPS_lod_configs(PropertyGroup):
+    lod_number: IntProperty(name='Number of LODs', default=2, min=0, max=10)
 
 class BITCAKE_OT_dev_operator(Operator):
     bl_idname = "bitcake.dev_operator"
@@ -9,18 +11,50 @@ class BITCAKE_OT_dev_operator(Operator):
     bl_description = "Test stuuuuff"
     bl_options = {'INTERNAL', 'UNDO'}
 
+    lod_number: IntProperty(default=0)
+    lod_ratio: FloatProperty(default=1)
+
     @classmethod
     def poll(cls, context):
         return True
 
     def execute(self, context):
         obj = context.active_object
+        selection = context.selected_objects
 
-        print(obj)
+        for obj in selection:
+            if not obj.get('LOD'):
+                obj['LOD'] = 0
 
-        obj_copy = duplicate(obj)
-        decimate = obj_copy.modifiers.new('Decimate', 'DECIMATE')
-        decimate.ratio = 0.5
+        scene_data = bpy.data.objects
+        for child in obj.children_recursive:
+            if child.get('LOD'):
+                scene_data.remove(child)
+
+        for lod in range(self.lod_number):
+            if obj.get('LOD') is not 0:
+                continue
+
+            obj_copy = duplicate(obj)
+            current_lod = lod + 1
+
+            is_lod = obj_copy.get('LOD')
+            if not is_lod:
+                obj_copy['LOD'] = current_lod
+
+            print(obj_copy.name)
+            obj_name = obj_copy.name.split('.')
+            obj_copy.name = obj_name[0] + '_LOD' + str(current_lod)
+
+            decimate = obj_copy.modifiers.get('Decimate')
+            if not decimate:
+                decimate = obj_copy.modifiers.new('Decimate', 'DECIMATE')
+
+            self.lod_ratio = self.lod_ratio / 2
+            decimate.ratio = self.lod_ratio
+
+        self.lod_ratio = 1
+
 
         return {'FINISHED'}
 
@@ -42,48 +76,51 @@ def duplicate(obj, data=True, actions=True, parent=True):
     return obj_copy
 
 
-def construct_registered_project_published_export_directory(self):
-    blend_path = Path(bpy.path.abspath('//'))
-    pathway = []
-
-    # If no WIP folder found then fail
-    if not is_wip_in_path():
-        self.report({"ERROR"},
-                    "The .blend path is not contained inside a proper BitCake Pipeline hierarchy, please make sure your hierarchy's root folder contains the word 'WIP' like in c:/BitTools/02_WIP/Environment")
-        return {'CANCELLED'}
-
-    # Search the .blend Path for BitCake's folder structure
-    # Change _WIP folder to Art then construct the rest of the path
-    for part in blend_path.parts:
-        pathway.append(part)
-        if part.__contains__('02_WIP'):
-            pathway.pop()
-            pathway.append('03_Published')
-
-
-    # Construct final directory and return it
-    constructed_directory = Path().joinpath(*pathway) # Unpacks the list as arguments
-
-    return constructed_directory
-
-
 def draw_panel(self, context):
+    lod_configs = context.scene.lod_configs
+
     layout = self.layout
 
     row = layout.row()
-    row.operator('bitcake.dev_operator', text='Test Butten')
+    row.prop(lod_configs, 'lod_number')
+    row = layout.row()
+    op = row.operator('bitcake.dev_operator', text='Generate LODs')
+    op.lod_number = lod_configs.lod_number
+
+    layout.separator()
+    obj = context.active_object
+
+    if obj is None:
+        return
+
+    mod = obj.modifiers.get('Decimate')
+    if mod is not None:
+        row = layout.row()
+        row.prop(mod, 'ratio', text=f"LOD{str(obj.get('LOD'))}")
+
+    for child in obj.children_recursive:
+        mod = child.modifiers.get('Decimate')
+        if mod is not None:
+            row = layout.row()
+            row.prop(mod, 'ratio', text=f"LOD{str(child.get('LOD'))}")
 
     return
 
 
-classes = (BITCAKE_OT_dev_operator,)
+classes = (BITCAKE_OT_dev_operator,
+           BITCAKE_PROPS_lod_configs)
 
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
+    Scene.lod_configs = bpy.props.PointerProperty(type=BITCAKE_PROPS_lod_configs)
+
+
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+
+    del Scene.lod_configs
