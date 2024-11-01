3317ccf97d5dde79fc0850c8ca10d44f555be615
vamolessa <1696072+vamolessa@users.noreply.github.com>
2024-11-01 18:57:21 -0300

    pequeno fix quando tenta exportar uma hierarquia com empty no começo (e possivelmente no meio também)

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -136,7 +136,7 @@ class BITCAKE_OT_universal_exporter(Operator):
                 bpy.ops.object.make_single_user(object=True, obdata=True, material=False, animation=False, obdata_animation=True)
 
             if panel_prefs.apply_transform:
-                if not [armature for armature in obj.modifiers if armature.type == 'ARMATURE']:
+                if not [armature for armature in obj.modifiers if armature.type == 'ARMATURE'] and obj.data:
                     original_data_name = obj.data.name
                     original_data = obj.data.copy()
                     obj_original_info_dict[obj]['original_data_name'] = original_data_name
